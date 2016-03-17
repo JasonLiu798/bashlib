64bf6818d7a2ca3c316ca4872a174de6740e5565
JasonLiu798 <jasondliu@qq.com>
2016-03-17 17:19:42 +0800

    update

--- bashrc/win/cmdrc.bat
+++ bashrc/win/cmdrc.bat
@@ -1,5 +1,7 @@
 @doskey ls=dir /b $*
 @doskey l=dir /od/p/q/tw $*
 @doskey ping=ping -t $*
+@doskey cat=TYPE $*
+@doskey vi=sublime_text $*
 ::d:
 ::cd d:\yp\project
---
+++ lib/dateUtil.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+function date2timestamp()
+{
+
+}
+#date -d "2010-10-18 00:00:00" +%s
